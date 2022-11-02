fbcd4b545ae2cc9e68f7e4c0db9d739b9874e325
Luis <luis.borsoi@sap.com>
2022-11-02 01:22:47 -0300

    add anonymous login

--- quizapp/lib/home/home.dart
+++ quizapp/lib/home/home.dart
@@ -1,34 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:quizapp/login/login.dart';
+import 'package:quizapp/shared/shared.dart';
+import 'package:quizapp/topics/topics.dart';
+import 'package:quizapp/services/auth.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-          Center(
-            child: ElevatedButton(
-              child: Text(
-                'about',
-                style: Theme.of(context).textTheme.button,
-              ),
-              onPressed: () => Navigator.pushNamed(context, '/about'),
-            ),
-          ),
-          Center(
-            child: ElevatedButton(
-              child: Text(
-                'topics',
-                style: Theme.of(context).textTheme.button,
-              ),
-              onPressed: () => Navigator.pushNamed(context, '/topics'),
-            ),
-          ),
-        ],
-      ),
+    return StreamBuilder(
+      stream: AuthService().userStream,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const LoadingScreen();
+        } else if (snapshot.hasError) {
+          return const Center(
+            child: ErrorMessage(),
+          );
+        } else if (snapshot.hasData) {
+          return const TopicsScreen();
+        } else {
+          return const LoginScreen();
+        }
+      },
     );
   }
 }
--- quizapp/lib/profile/profile.dart
+++ quizapp/lib/profile/profile.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:quizapp/services/auth.dart';
 
 class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({Key? key}) : super(key: key);
+  const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text('Profile'),
+      ),
+      body: ElevatedButton(
+          child: const Text('sign out'),
+          onPressed: () async {
+            await AuthService().signOut();
+            Navigator.of(context)
+                .pushNamedAndRemoveUntil('/', (route) => false);
+          }),
     );
   }
 }
