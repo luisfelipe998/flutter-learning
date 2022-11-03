aa8a3077723e30bad49dc0de1c0bf1f440df3531
Luis <luis.borsoi@sap.com>
2022-11-02 23:17:42 -0300

    add topics screen. Add individual topic screen with hero animation.

--- quizapp/lib/services/auth.dart
+++ quizapp/lib/services/auth.dart
@@ -45,7 +45,6 @@ class AuthService {
         .join();
   }
 
-  /// Returns the sha256 hash of [input] in hex notation.
   String sha256ofString(String input) {
     final bytes = utf8.encode(input);
     final digest = sha256.convert(bytes);
@@ -53,14 +52,9 @@ class AuthService {
   }
 
   Future<UserCredential> signInWithApple() async {
-    // To prevent replay attacks with the credential returned from Apple, we
-    // include a nonce in the credential request. When signing in with
-    // Firebase, the nonce in the id token returned by Apple, is expected to
-    // match the sha256 hash of `rawNonce`.
     final rawNonce = generateNonce();
     final nonce = sha256ofString(rawNonce);
 
-    // Request credential for the currently signed in Apple account.
     final appleCredential = await SignInWithApple.getAppleIDCredential(
       scopes: [
         AppleIDAuthorizationScopes.email,
@@ -69,14 +63,10 @@ class AuthService {
       nonce: nonce,
     );
 
-    // Create an `OAuthCredential` from the credential returned by Apple.
     final oauthCredential = OAuthProvider("apple.com").credential(
       idToken: appleCredential.identityToken,
       rawNonce: rawNonce,
     );
-
-    // Sign in the user with Firebase. If the nonce we generated earlier does
-    // not match the nonce in `appleCredential.identityToken`, sign in will fail.
     return await FirebaseAuth.instance.signInWithCredential(oauthCredential);
   }
 
