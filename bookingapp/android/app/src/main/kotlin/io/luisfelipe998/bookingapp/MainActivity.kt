dc57effd5c24830f72f0831663569652481336c8
Luis <luis.borsoi@sap.com>
2022-11-12 00:09:13 -0300

    add main screen

---
+++ bookingapp/android/app/src/main/kotlin/io/luisfelipe998/bookingapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.luisfelipe998.bookingapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ bookingapp/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:bookingapp/screens/bottom_bar.dart';
+import 'package:bookingapp/utils/app_styles.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: primary,
+      ),
+      home: const BottomBar(),
+    );
+  }
+}
